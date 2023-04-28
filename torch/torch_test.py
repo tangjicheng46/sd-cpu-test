164f911e397fb4c4b028f20ee82a1e3c6599f454
TangJicheng <tang.j@ctw.inc>
2023-04-28 12:34:12 +0900

    update

--- torch/torch_test.py
+++ torch/torch_test.py
@@ -1,5 +1,6 @@
 import torch
 from diffusers import DiffusionPipeline
+import time 
 
 pipe = DiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5",
@@ -8,4 +9,12 @@ pipe = pipe.to("cpu")
 
 prompt = "a photo of an astronaut riding a horse on mars"
 pipe.enable_attention_slicing()
+
+start = time.time()
 image = pipe(prompt).images[0]
+end = time.time()
+print("cost: ", end - start)
+
+
+print(image)
+image.save("./1.jpg")
