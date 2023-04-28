db793f3c378dbb3b1eb73007bb28f06bc82e0234
TangJicheng <tang.j@ctw.inc>
2023-04-28 14:38:12 +0900

    add gpu

---
+++ ort/ort_gpu_test.py
@@ -0,0 +1,9 @@
+from optimum.onnxruntime import ORTStableDiffusionPipeline
+
+model_id = "sd_v15_onnx"
+pipe = ORTStableDiffusionPipeline.from_pretrained(model_id)
+
+pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+images = pipe(prompt).images[0]
---
+++ torch/torch_gpu_test.py
@@ -0,0 +1,26 @@
+import torch
+from diffusers import DiffusionPipeline
+import time 
+
+pipe = DiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+)
+pipe = pipe.to("cuda")
+
+pipe.save_pretrained("./model1")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+pipe.enable_attention_slicing()
+
+torch.cuda.synchronize()
+start = time.time()
+
+image = pipe(prompt).images[0]
+
+torch.cuda.synchronize()
+end = time.time()
+print("cost: ", end - start)
+
+
+print(image)
+image.save("./1.jpg")
