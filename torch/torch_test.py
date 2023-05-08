7b20c31fca6f7d04c1c26a6db82c67be8eb3d02f
TangJicheng <tang.j@ctw.inc>
2023-05-08 17:59:27 +0900

    update

--- torch/torch_test.py
+++ torch/torch_test.py
@@ -3,22 +3,13 @@ import argparse
 
 import torch
 from diffusers import DiffusionPipeline, StableDiffusionPipeline
+from optimum.onnxruntime import ORTStableDiffusionPipeline
 
 
-def run_inference(model_path: str,
-                  prompt: str,
-                  device: str,
-                  is_save: bool = True):
-    pipe = StableDiffusionPipeline.from_pretrained(model_path)
+def run_inference(pipe, prompt: str, device: str, is_save: bool = True):
     pipe = pipe.to(device)
-
-    torch.cuda.synchronize()
     start = time.time()
-
-    # inference
     image = pipe(prompt=prompt, num_inference_steps=20).images[0]
-
-    torch.cuda.synchronize()
     end = time.time()
     print("cost: ", end - start)
     print(image)
@@ -30,15 +21,32 @@ if __name__ == "__main__":
     prompt = "a photo of an astronaut riding a horse on mars"
 
     parser = argparse.ArgumentParser(prog="Stable Diffusion Performance Test")
-
-    parser.add_argument(
-        "--model_path",
-        default="../stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt",
-        type=str)
+    parser.add_argument("--model_path",
+                        default="runwayml/stable-diffusion-v1-5",
+                        type=str)
+    parser.add_argument("--engine", default="torch", type=str)
     parser.add_argument("--device", default="cpu", type=str)
+    parser.add_argument("--dtype", default="fp32", type=str)
 
     args = parser.parse_args()
 
-    run_inference(model_path=args.model_path,
-                  prompt=prompt,
-                  device=args.device)
+    infer_dtype = torch.float32
+    if args.dtype == "fp16":
+        infer_dtype = torch.float16
+
+    pipe = None
+    if args.engine == "ort":
+        pipe = ORTStableDiffusionPipeline.from_pretrained(
+            args.model_path, torch_dtype=infer_dtype)
+        pipe.save_pretrained("./my_onnx")
+    else:
+        pipe = StableDiffusionPipeline.from_pretrained(args.model_path,
+                                                       torch_dtype=infer_dtype)
+        pipe.enable_attention_slicing()
+        pipe.enable_vae_slicing()
+        pipe.enable_vae_tiling()
+        pipe.save_pretrained("./my_torch")
+
+    N = 2
+    for i in range(N):
+        run_inference(pipe, prompt=prompt, device=args.device)
