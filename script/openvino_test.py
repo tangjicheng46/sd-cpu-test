ae552c27f42e56c3b3ba7f561f4c5bc9eea0fa3e
TangJicheng123 <130357271+TangJicheng123@users.noreply.github.com>
2023-05-10 16:51:58 +0900

    update vino test

--- script/openvino_test.py
+++ script/openvino_test.py
@@ -1,13 +1,31 @@
 from optimum.intel.openvino import OVStableDiffusionPipeline
 import time
 
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = OVStableDiffusionPipeline.from_pretrained(model_id, export=True)
-prompt = "a photo of an astronaut riding a horse on mars"
 
-start = time.time()
-images = pipe(prompt=prompt, num_inference_steps=20).images[0]
-images.save("./1.jpg")
-end = time.time()
+def run_vino_inference(prompt: str, is_save: bool = False):
+    model_id = "runwayml/stable-diffusion-v1-5"
+    pipe = OVStableDiffusionPipeline.from_pretrained(model_id, export=True)
 
-print("cost: ", end - start)
+    # warmup
+    image = pipe(prompt=prompt, num_inference_steps=20).images[0]
+
+    # run loop for performance testing
+    loop = 2
+    total_cost_time = 0.0
+    for i in range(loop):
+        start = time.time()
+        image = pipe(prompt=prompt, num_inference_steps=20).images[0]
+        end = time.time()
+        cost_time = end - start
+        total_cost_time += cost_time
+        print(f"[{i}] cost: {cost_time}")
+        if is_save:
+            image.save("./vino_generated_image.jpg")
+    print("Performance test is over.")
+    print(f"Average cost is {total_cost_time / loop}")
+
+
+if __name__ == "__main__":
+    prompt = "a photo of an astronaut riding a horse on mars"
+
+    run_vino_inference(prompt=prompt)
