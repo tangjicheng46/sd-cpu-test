8475ff4cf6bd6866d77f8608c7073352d2d5285a
TangJicheng123 <130357271+TangJicheng123@users.noreply.github.com>
2023-05-10 16:35:21 +0900

    delete useless file

--- ort/ort_save.py
+++
@@ -1,7 +0,0 @@
-from optimum.onnxruntime import ORTStableDiffusionPipeline
-
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = ORTStableDiffusionPipeline.from_pretrained(model_id, export=True)
-prompt = "a photo of an astronaut riding a horse on mars"
-images = pipe(prompt).images[0]
-pipe.save_pretrained("./onnx-stable-diffusion-v1-5")
